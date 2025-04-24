d57b18ac2b22974a4ea75b193f5e3bb86a0a9b12
Megan Kossa <dragon.flame.meganite+github@gmail.com>
2025-04-23 20:46:49 -0500

    Added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { QuickLinksLanding } from './components/LandingPages';
+// import './App.css'
+import { QuickLinksLanding } from './components/LandingPages.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
