ea1d8cf9b99223b8bb52ae52802d9ceb0bfc1524
Megan Kossa <dragon.flame.meganite+github@gmail.com>
2025-05-06 20:24:11 -0500

    Add some breakpoints, fix content, tweak colors

--- src/components/LandingPages.tsx
+++ src/components/LandingPages.tsx
@@ -10,14 +10,15 @@ function QuickLinksLanding(){
                 <div className="logoGlyph">{MeganopteryxLogo()}</div>
                 <h1 className="logoText">Meganopteryx<br className="hidden md:visible"/>Design</h1></div>
             <div className="slogan">{SloganRoulette()}</div>
+            <div className="cardContainer">
             <div className="card">
                 <h2>See what I've made!</h2>
                 <ul>
                     <li><a href="https://github.com/meganopteryx">Github</a></li>
-                    <li><a href="https://www.inaturalist.org/people/meganopteryx">iNaturalist</a></li>
+                    <li><a href="https://www.inaturalist.org/people/meganopteryx">iNaturalist</a> (My photography)</li>
                     <li><a href="https://www.instagram.com/meganopteryx/">Instagram</a></li>
-                    <li><a href="https://www.tumblr.com/meganopteryx">Tumblr (probably has the most art content, but somewhat old)</a></li>
-                    <li><a href="https://www.deviantart.com/meganbednarz">Deviantart (EVEN OLDER art)</a></li>
+                    <li><a href="https://www.tumblr.com/meganopteryx">Tumblr</a> (probably has the most art content, but somewhat old)</li>
+                    <li><a href="https://www.deviantart.com/meganbednarz">Deviantart</a> (EVEN OLDER art)</li>
                     <li><a href="https://meganopteryx.com/archive">Ye Olde Website</a> Warning: SUPER OLD DESIGN AND NOT MOBILE FRIENDLY</li>
                 </ul>
             </div>
@@ -26,6 +27,8 @@ function QuickLinksLanding(){
                 <ul>
                     <li><a href="https://www.teepublic.com/user/meganopteryx">Teepublic</a></li>
                     <li><a href="https://meganopteryx.etsy.com">Etsy Shop</a></li> 
+                    <li><a href="mailto:meganopteryx@gmail.com">Commission me!</a></li> 
+                    {/* TODO: Make the commission form */}
                 </ul>
             </div>
             <div className="card">
@@ -36,7 +39,7 @@ function QuickLinksLanding(){
                     <li><a href="https://www.twitch.tv/meganopteryx">Twitch (for the occasional stream)</a></li>
                     <li>Discord @Meganopteryx</li>
                 </ul>
-            </div>
+            </div></div>
 {/*             
             <h2>Quick Links</h2>
             <ul>
@@ -51,13 +54,13 @@ function QuickLinksLanding(){
                 <li><a href="https://www.deviantart.com/meganbednarz">Deviantart (EVEN OLDER art)</a></li>
             </ul> */}
             {/* <p>Warning: Here be dragons.... do you dare enter <a href="/archive">ye olde website???</a> (not all content migrated here yet!)</p> */}
-            <h2>Chatter</h2>
+            {/* <h2>Chatter</h2>
             <h3>What I'm up to nowadays</h3>
             <p>Let's see... I got into bullet journaling and started collecting Way Too Many stickers, but some of the pages turned out pretty good. I used to do resin casting but haven't had much time for it lately, as it seems I need chunks of uninterrupted time which just doesn't happen as a mom of young kids. I'm doing occasional art commissions and attempting to break into the world of illustration but it's slow going so far. Dipping my feet back into programming, as evidenced here. We got a 3D printer for Christmas and HOLY CRAP I forgot how much work 3D modeling is. I'm much better at painting, so I've been printing things I find and painting them afterwards. It's pretty fun! I might get into tabletop wargaming at some point.</p>
             <p>Otherwise I'm mostly trying to be a good mom of an autistic 5yo and a stay-at-home 1yo, and a good wife for a somewhat-overworked programmer husband. For breaks I do some drawing and crafting and nature photography (which I usually post on <a href="">iNaturalist!</a>). And of course, video games. Can't forget the video games.</p>
             <h3>Where am I most active</h3>
-            <p>Discord, no doubt about it. As usual, you can find me as Meganopteryx. Here are a few of my favorite public discords. If you share some of my interests I'd love to see you there! {/* INSERT LINKS HERE */}I'm also occasionally on FFXIV, Crystal DC. You can also find me on Steam. Just plain old texting works great if you know my number! The most consistent way to message me, especially in long-form is email, as follows: meganopteryx at gmail dot com</p>
-            <p>See you at church on Sunday, and at the Rattler's Den on Wednesday!</p>
+            <p>Discord, no doubt about it. As usual, you can find me as Meganopteryx. Here are a few of my favorite public discords. If you share some of my interests I'd love to see you there! I'm also occasionally on FFXIV, Crystal DC. You can also find me on Steam. Just plain old texting works great if you know my number! The most consistent way to message me, especially in long-form is email, as follows: meganopteryx at gmail dot com</p>
+            <p>See you at church on Sunday, and at the Rattler's Den on Wednesday!</p> */}
         </div>
     )
 };
